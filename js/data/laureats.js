8cc1dd32d484c4f767c1632760f4d26270e1a8d9
Noha Boussouf <nounaboussouf@gmail.com>
2020-06-26 19:41:10 +0100

    ajout des données

--- js/data/laureats.js
+++ js/data/laureats.js
@@ -1,31 +1,35 @@
 var laureats = [
     new Laureat(
         "EL MORABET", "Hicham", "elmorabethicham@gmail.com", "2017-2019", "Umanis, Casablanca",
-         "Umanis, Casablanca", "Capgemini", "Stagiaire", "France", "null", ""
+         "Umanis, Casablanca", "Capgemini", "Stagiaire", "France", "images/Laureats/Hicham Elmorabet.jpg", ""
     ),
     new Laureat(
-        "El KHAIRY", "Ibtissam ", "elkhairy.ibtissam96@gmail.com", "2017-2019", "Cegedim, Casablanca",
-         "Cegedim, Casablanca", "BITS", "Ingénieur développement logiciel ", "France", "null", ""
-    ),
-    new Laureat(
-        "HAMMIOUI", "Aymane ", "aymane.hammioui@gmail.com", "2016-2018", "Cegedim, Casablanca",
-         "Umanis, Casablanca", "Salesforce", "Consultant Technique", "Casablanca", "null", ""
-    ),
-    new Laureat(
-        "HAIDAR", "Hicham ", "hichamhaidar96@gmail.com", "2018-2020", "CGI, Fès",
-         "aucune", "CGI, Fès", "Stagiaire", "Fés", "null", ""
+        "EL MANSOUR", "RIAD", "el.mansour@hotmail.fr", "2016-2018", "Capgemini, Casablanca", 
+        "Capgemini", "Royale Marocaine d'Assurances , Casablanca", "Ingénieur d'études et de développement, MOE ", "casablanca", "images/Laureats/Riad Elmansour.jpg", ""
     ),
     new Laureat(
         "YOUSSEF", "Mouad ", "imouad.you@gmail.com", "2018-2020", "SQLI, Rabat",
-         "aucune", "SQLI, Rabat", "Ingénieur concepteur développeur", "Rabat", "null", ""
+         "aucune", "SQLI, Rabat", "Ingénieur concepteur développeur", "Rabat", "images/Laureats/Mouad Youssef.jpg", ""
+    ),
+    new Laureat(
+        "El KHAIRY", "Ibtissam ", "elkhairy.ibtissam96@gmail.com", "2017-2019", "Cegedim, Casablanca",
+         "Cegedim, Casablanca", "BITS", "Ingénieur développement logiciel ", "France", "images/Laureats/Ibtissam El khairy.jpg", ""
+    ),
+    new Laureat(
+        "HAMMIOUI", "Aymane ", "aymane.hammioui@gmail.com", "2016-2018", "Cegedim, Casablanca",
+         "Umanis, Casablanca", "Salesforce", "Consultant Technique", "Casablanca", "images/Laureats/Aymane Hammioui.jpg", ""
+    ),
+    new Laureat(
+        "HAIDAR", "Hicham ", "hichamhaidar96@gmail.com", "2018-2020", "CGI, Fès",
+         "aucune", "CGI, Fès", "Stagiaire", "Fés", "images/Laureats/Hicham Haidar.jpg", ""
     ),
     new Laureat(
         "FILALI SADIQ", "Safae ", "safae.filali.sadiq@gmail.com", "2016-2018", "Capgemini",
-         "Capgemini", "Sofrecom", "Concepteur devloppeur", "Rabat", "images/Laureats/Filali Sadiq.jpg", ""
+         "Capgemini", "Sofrecom", "Concepteur devloppeur", "Rabat", "images/Laureats/Safae Filali Sadiq.jpg", ""
     ),
     new Laureat(
         "EL HAKMI", "Ghita ", "ghita.lvsl@gmail.com", "2017-2019", "Capgemini, Rabat",
-         "Capgemini, Rabat", "Capgemini, Rabat", "Software Engineer", "Rabat", "null", ""
+         "Capgemini, Rabat", "Capgemini, Rabat", "Software Engineer", "Rabat", "images/Laureats/Ghita Elhakmi.jpg", ""
     ),
     new Laureat(
         "ZERKOUN", "Fatima ", "zerkoun.fatima@gmail.com", "2018-2020", "CGI",
@@ -41,31 +45,27 @@ var laureats = [
     ),
     new Laureat(
         "LAALAHMI", "Chaimae", "chaimae.laalahmi@gmail.com", "2017-2019", "Capgemini, Rabat",
-         "Capgemini, Rabat", "Capgemini, Rabat", "Software Engineer", "Rabat", "null", ""
+         "Capgemini, Rabat", "Capgemini, Rabat", "Software Engineer", "Rabat", "images/Laureats/Chaimae Laalahmi.jpg", ""
     ),
     new Laureat(
         "MAJDOULIF", "Anouar", "anouarmajdoulif@gmail.com", "2017-2019", "Capgemini, Casablanca",
-         "Capgemini, Casablanca", "Leyton", "Technical Consultant", "Casablanca", "null", ""
+         "Capgemini, Casablanca", "Leyton", "Technical Consultant", "Casablanca", "images/Laureats/Anouar Majdoulif.jpg", ""
     ),
     new Laureat(
         "Mestadi", "Hala", "driouich.fatimaezzahrae@gmail.com", "2018-2020", "DXC Technology , casablanca", 
-        "aucune", "capgemini, Rabat", "Ingénieur concepteur développeur", "Rabat", "null", ""
+        "aucune", "capgemini, Rabat", "Ingénieur concepteur développeur", "Rabat", "images/Laureats/Hala Mestadi.jpg", "https://www.linkedin.com/in/hala-mestadi/"
     ),
     new Laureat(
         "DRIOUICH", "Fatima ez-zahrae", "hmestadi@sqli.com", "2018-2020", "SQLI, Rabat", 
-        "aucune", "SQLI, Rabat", "Software Engineer", "Rabat", "null", ""
+        "aucune", "SQLI, Rabat", "Software Engineer", "Rabat", "images/Laureats/fatima ez-zahrae driouich.jpg", ""
     ),
     new Laureat(
         "MAHBOUB", "Marwa", "mahboub.marwa.96@gmail.com", "2017-2019", "Capgemini, Casablanca", 
         "Capgemini", "Capgemini, Casablanca", "Ingénieure d'études et développement", "casablanca", "null", ""
     ),
     new Laureat(
-        "MEL MANSOUR", "RIAD", "el.mansour@hotmail.fr", "2016-2018", "Capgemini, Casablanca", 
-        "Capgemini", "Royale Marocaine d'Assurances , Casablanca", "Ingénieur d'études et de développement, MOE ", "casablanca", "null", ""
-    ),
-    new Laureat(
         "BELARBI", "Ahmed", "belarbi.ahmed21@gmail.com", "2016-2018", "Capgemini, Casablanca", 
-        "Capgemini", "capgemini, Casablanca", "Software engineer PL/SQL", "casablanca", "null", ""
+        "Capgemini", "capgemini, Casablanca", "Software engineer PL/SQL", "casablanca", "images/Laureats/Ahmed Belarbi.jpg", ""
     ),
     new Laureat(
         "YACOUBI", "Nizar", "nizar.yakoubi@usmba.ac.ma", "2017-2019", "CGI", 
@@ -73,23 +73,23 @@ var laureats = [
     ),
     new Laureat(
         "BOUZRIA", "Souhayla", "bouzria.souhayla@gmail.com", "2017-2019", "Capgemini, Casablanca", 
-        "Capgemini", "capgemini, Casablanca", "Software engineer", "casablanca", "null", ""
+        "Capgemini", "capgemini, Casablanca", "Software engineer", "casablanca", "images/Laureats/Souhayla Bouzria.jpg", ""
     ),
     new Laureat(
         "ZEROUAL ", "Badr", "badr.zer@gmail.com", "2012-2014", "Alten", 
-        "Capgemini", "Total Direct Energie", "Coach agile", "France", "null", ""
+        "Capgemini", "Total Direct Energie", "Coach agile", "France", "images/Laureats/Badr Zeroual.jpg", ""
     ), 
     new Laureat(
         "CHRAIBI ", "Mouna", "chr.mouna@gmail.com", "2017-2019", "Umanis", 
-        "Umanis", "Umanis", "Consultante BI", "casablanca", "null", ""
+        "Umanis", "Umanis", "Consultante BI", "casablanca", "images/Laureats/Mouna Chraibi.jpg", ""
     ), 
     new Laureat(
         "EL Mansour", "Salma", "selma.elmansour@gmail.com", "2016-2018", "Capgemeni ", 
-        "Capgemeni ", "Capgemeni ", "Software engineer", "casablanca", "null", ""
+        "Capgemeni ", "Capgemeni ", "Software engineer", "casablanca", "images/Laureats/Salma El Mansour.jpg", ""
     ),
     new Laureat(
-        "GAMRAN", "Ayoub", "gamrani.ayoub@gmail.com", "2016-2018", "Cgi", 
-        "Cgi ", "Bluescale", "Consultant Java JEE", "France", "null", ""
+        "GAMRANI", "Ayoub", "gamrani.ayoub@gmail.com", "2016-2018", "Cgi", 
+        "Cgi ", "Bluescale", "Consultant Java JEE", "France", "images/Laureats/Ayoub Gamrani.jpg", ""
     ),
     new Laureat(
         "EL GADDAR", "Chaimae", "elgaddarchaimae@gmail.com", "2017-2019", "Capgemeni ", 
@@ -97,7 +97,7 @@ var laureats = [
     ),
     new Laureat(
         "NABET", "Wafae", "nabetwafae@gmail.com", "2018-2020", "CEGEDIM", 
-        "CEGEDIM", "aucune", "Consultante en systèmes d’information de ressources humaines", "rabat", "null", ""
+        "CEGEDIM", "aucune", "Consultante en systèmes d’information de ressources humaines", "rabat", "images/Laureats/Wafae Nabet.jpg", ""
     ), 
     new Laureat(
         "AALOUCH", "Hind", "aalouch.hind.inside@gmail.com", "2018-2020", "Spacebyte AG", 
@@ -105,22 +105,22 @@ var laureats = [
     ), 
     new Laureat(
         "MOUHIM", "Youness", "youness.mouhim1@gmail.com", "2015-2017", "Cgi", 
-        "Cgi", "Vivaliente Inc", "Consultant Oracle applications confirmé", "France", "null", ""
+        "Cgi", "Vivaliente Inc", "Consultant Oracle applications confirmé", "France", "images/Laureats/Youness Mouhim.jpg", ""
     ),
     new Laureat(
         "Wirzgane", "Daoud", "daoud.wirzgane@outlook.com", "2018-2020", "Intelcia", 
-        "null", "Intelcia", "Analyse programmeur", "casablanca", "null", ""
+        "null", "Intelcia", "Analyse programmeur", "casablanca", "images/Laureats/Daoud Wirzgane.jpg", ""
     ),
     new Laureat(
         "El Guarti", "Abdelhamid", "abdelhamid.elguarti@usmba.ac.ma", "2014-2016", "CEGEDIM", 
-        "CEGEDIM", "Menisys", "Software engineer", "France", "null", ""
+        "CEGEDIM", "Menisys", "Software engineer", "France", "images/Laureats/El Guarti Abdelhamid.jpg", ""
     ),                   
     new Laureat(
         "DARBOUZ", "Soufiane", "galaxy.darbouz@gmail.com", "2018-2020", "SQLI", 
-        "aucune", "SQLI", "Front-end Engineer", "rabat", "null", ""
+        "aucune", "SQLI", "Front-end Engineer", "rabat", "images/Laureats/Soufiane Darbouz.jpg", ""
     ),
     new Laureat(
         "BOUARAIS", "Salma", "salma.bouarais.97@gmail.com", "2018-2020", "SQLI", 
-        "aucune", "aucune", "aucune", "aucune", "null", ""
+        "aucune", "aucune", "aucune", "aucune", "images/Laureats/Salma Bouarais.jpg", ""
     )   
 ];
