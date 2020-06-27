85835b17096476ccd5e13d13658aa785f9116eb1
Alphanionight <houssamtheflash@gmail.com>
2020-06-27 01:39:23 +0100

    supprission des anciens fichiers

--- js/model/Event.js
+++
@@ -1,9 +0,0 @@
-function Evnt(image, label, text, day, month, hour, minute){
-    this.image = image;
-    this.label = label;
-    this.text = text;
-    this.day = day;
-    this.month = month;
-    this.hour = hour;
-    this.minute = minute;
-}
